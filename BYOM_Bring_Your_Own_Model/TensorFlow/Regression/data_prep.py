364ecc8b45729fc643c33a79f0fc456842f20116
Jose Sifontes <jsifontes@jsifont-ltmjgjf.internal.salesforce.com>
2022-11-10 17:44:04 -0500

    Add Model Files

---
+++ BYOM_Bring_Your_Own_Model/TensorFlow/Regression/data_prep.py
@@ -0,0 +1,36 @@
+# **************************************************************************************************
+# AUTHOR: Jose Sifontes
+# ORIGIN: 2022-11-08
+# PURPOSE: Create quick script to prepare the validation file for Model Manager upload
+# STEPS:
+# 1) Import necessary libraries
+# 2) Read validation dataset (99 records) not containing target variable, execute read_dataset function against it
+# 3) Loan the serialized model and run predictions on processed dataset
+# 4) Crate a validation dataset that takes the original dataset and append the predictions, output to csv as validation.csv
+# **************************************************************************************************
+
+# Import Packages
+import pandas as pd
+from tensorflow.keras.models import load_model
+import data_processor as dp
+
+# Load serialized model
+filename = './saved_model.pb'
+loaded_model = load_model(filename)
+
+# Read and process validation base dataset with no target variable
+df_source = pd.read_csv('validation_bk.csv')
+df = dp.read_dataset('validation_bk.csv')
+df.info()
+
+# Predict target on dataset
+predictions = loaded_model.predict(df)
+print(predictions)
+
+# Define dataframes to append and create new dataframe for validation
+target = pd.DataFrame(predictions, columns=['Churned_c'])
+validation = pd.concat([df_source,target], axis=1)
+print(validation)
+
+# Write results to validation.csv file which will be used to upload to Model Manager zip
+validation.to_csv('validation.csv', index=False)
---
+++ BYOM_Bring_Your_Own_Model/TensorFlow/Regression/data_processor.py
@@ -0,0 +1,47 @@
+# **************************************************************************************************
+# PROJECT: BYOM Data Preprocessor for Fuel Efficiency Regression model using TensorFlow
+# AUTHOR: Jose Sifontes
+# ORIGIN: 2022-05-13
+# PURPOSE: Create necessary pre/post processing functions for BYOM functionality
+# STEPS:
+# 1) Specify LABEL_COLUMN to indicate outcome variable/field (required)
+# 2) Define preprocessor() to handle data preprocessing
+# 3) Define postprocessor() to tidy up data after running through model
+# **************************************************************************************************
+
+# Import necessary packages
+import pandas as pd
+import numpy as np
+
+# Define variable for target 
+LABEL_COLUMN = 'MPG'
+
+# Function to take in csv and read the dataset
+def read_dataset(file_path: str):
+    """This function reads the CSV file, then drops all observations with missing values, next, 
+    it converts categorical variables using one-hot encoding, and finally re-orders the index of the dataframe
+    """
+    raw_data = pd.read_csv(file_path)
+    digested_dataset = raw_data.dropna()
+    digested_dataset = digested_dataset.reindex(sorted(digested_dataset.columns),axis=1)
+    return digested_dataset
+
+# Function to process the dataset and apply any transformations
+def preprocessor(file_path: str):
+    """This function takes the dataframe that was read as input and drops the target variable if found, returning a clean set for training"""
+    dataset = read_dataset(file_path)
+    if LABEL_COLUMN in dataset.columns:
+        dataset.pop(LABEL_COLUMN)
+    return dataset.values
+
+
+# Function to process the predictions into a list for ingestion
+def postprocessor(predictions):
+    """
+    This is a mandatory function with a single input argument with array of predictions.
+    """
+    results = []
+    for prediction in predictions:
+        results.append(prediction[0])
+
+    return results
